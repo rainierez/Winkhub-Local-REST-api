1a887a0bbcecfe729c77ce9a05d587874b359a76
Chris <rainierez@yahoo.com>
2015-02-13 10:52:47 -0500

    Added support for 'New On/Off Output devices

--- www/statuscheck.sh
+++ www/statuscheck.sh
@@ -1,51 +1,73 @@
 #! /bin/bash
 PATH=$PATH:~/bin
 
-printf '{'
+function dimmable {
+  if aprontest -l -m $1 | awk '/Level/' | cut -c 100 | grep " " > /dev/null ;
+  then
+    LEVEL=$(aprontest -l -m $1 | awk '/Level/' | cut -c 101)
+  elif aprontest -l -m $1 | awk '/Level/' | cut -c 99 | grep " " > /dev/null ;
+  then
+    LEVEL=$(aprontest -l -m $1 | awk '/Level/' | cut -c 100-101)
+  else
+    LEVEL=$(aprontest -l -m $1 | awk '/Level/' | cut -c 99-101)
+  fi
 
-#get the level and status of the device passed into it if it is an 'HA Dimmable Light'
-function status {
-        if  aprontest -l -m $1 | awk '/HA Dimmable Light/'  | cut -c5-21 | grep 'HA Dimmable Light' > /dev/null ;
-        then
-                if [ $LOOP -eq 1 ] ;
-                then
-                        if [ $1 -gt 1 ] ;
-                        then
-                                printf '},'
-                        fi
-                fi
+  if aprontest -l -m $1 | awk '/On_Off/' | cut -c 95-102 | grep " ON " > /dev/null ;
+  then
+    STATUS="ON"
+  else
+    STATUS="OFF"
+  fi
 
-                if aprontest -l -m $1 | awk '/Level/' | cut -c 100 | grep " " > /dev/null ;
-                then
-                        LEVEL=$(aprontest -l -m $1 | awk '/Level/' | cut -c 101)
-                elif aprontest -l -m $1 | awk '/Level/' | cut -c 99 | grep " " > /dev/null ;
-                then
-                        LEVEL=$(aprontest -l -m $1 | awk '/Level/' | cut -c 100-101)
-                else
-                        LEVEL=$(aprontest -l -m $1 | awk '/Level/' | cut -c 99-101)
-                fi
-
-                if aprontest -l -m $1 | awk '/On_Off/' | cut -c 95-102 | grep " ON " > /dev/null ;
-                then
-                        STATUS="ON"
-                else
-                        STATUS="OFF"
-                fi
-
-                printf '"%s":{"Status": "%s","Level": "%s"' "$1" "$STATUS" "$LEVEL"
-        fi
+  printf '"%s":{"Type": "Dimmable Light","Status": "%s","Level": "%s"' "$1" "$STATUS" "$LEVEL"
 }
 
-#if you want to get multiple lights this starts at 1 and will go to a max number if passed or default to 25 if the optional parameter is not included
-if [ $1 == "all" ]; then
+function onoff {
+  if  aprontest -l -m $1 | awk '/On_Off/' | cut -c 100-102 | grep "ON" > /dev/null ;
+  then
+    STATUS="ON"
+  else
+    STATUS="OFF"
+  fi
 
-        for (( DEVICEID=1; DEVICEID<=${2:-25}; DEVICEID++ ))
-        do
-                LOOP=1
-                status $DEVICEID
-        done
+  printf '"%s":{"Type": "On/Off","Status": "%s"' "$1" "$STATUS"
+}
+
+if [ $1 == "all" ]; then
+  printf '{'
+  for (( DEVICEID=1; DEVICEID<=${2:-25}; DEVICEID++ ))
+  do
+    if  aprontest -l -m $DEVICEID | awk '/New HA Dimmable Light/' | grep 'New HA Dimmable Light' > /dev/null;
+    then
+      if [ $DEVICEID -gt 1 ] ;
+      then
+        printf '},'
+      fi
+      dimmable $DEVICEID
+    elif aprontest -l -m $DEVICEID | awk '/New On\/Off Output/' | grep 'New On\/Off Output' > /dev/null;
+    then
+      if [ $DEVICEID -gt 1 ] ;
+      then
+        printf '},'
+      fi
+      onoff $DEVICEID
+    fi
+  done
+  printf '}}'
+
 else
-        LOOP=0
-        status $1
+
+  if  aprontest -l -m $1 | awk '/New HA Dimmable Light/' | grep 'New HA Dimmable Light' > /dev/null;
+  then
+    printf '{'
+    dimmable $1
+    printf '}}'
+  elif aprontest -l -m $1 | awk '/New On\/Off Output/' | grep 'New On\/Off Output' > /dev/null;
+  then
+    printf '{'
+    onoff $1
+    printf '}}'
+  else
+    printf '{"Response":"Not a supported device or no device present"}'
+  fi
 fi
-printf '}}'
